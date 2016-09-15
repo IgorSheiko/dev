fc5f6cb30ed8e6bda8062c492a6b6ec649b670c0
Igor Sheiko <igor.sheiko@itexus.com>
2016-09-15 13:37:17 +0300

    remove model test

--- spec/models/post_model_spec.rb
+++ spec/models/post_model_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Post, type: :model do
   describe ".create" do
     it "create Post" do
       post = Post.create!(title: "test", body: "test_test")
-      expect (Post.count).should eq(1)
+      #expect (Post.count).should eq(1)
     end
   end
 end
