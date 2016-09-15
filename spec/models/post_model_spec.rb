16de9c6be6354c83fcb07e5c5e19aad3d48e6fa3
Igor Sheiko <igor.sheiko@itexus.com>
2016-09-15 13:19:19 +0300

    add test model

---
+++ spec/models/post_model_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe Post, type: :model do
+  describe ".create" do
+    it "create Post" do
+      post = Post.create!(title: "test", body: "test_test")
+      expect (Post.count).should eq(1)
+    end
+  end
+end
